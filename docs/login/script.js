27ead227d4e55c641597328c0995293499c75b45
William <theblazingexpert@gmail.com>
2024-03-20 17:51:02 +0700

    updated the html preview and removed some args
    removed "action" from login page

---
+++ docs/login/script.js
@@ -0,0 +1,7 @@
+document.querySelector('.img__btn').addEventListener('click', function() {
+  document.querySelector('.cont').classList.toggle('s--signup');
+});
+
+function myFunction() {
+  alert("That Feature Not Implemented Yet, Pls Try Another Method");
+}
